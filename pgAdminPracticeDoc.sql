705a19ca492941dacf699f4023aba787cf938ca1
Juan <46148241+JSaas9364@users.noreply.github.com>
2025-03-14 18:10:12 -0400

    updates

---
+++ pgAdminPracticeDoc.sql
@@ -0,0 +1,12 @@
+--SELECT * FROM payment
+--SELECT * FROM customer 
+--WHERE first_name ILIKE 'J%' AND last_name ILIKE 'S%'
+--WHERE first_name ILIKE '_her%' --AND last_name ILIKE 'S%'
+--WHERE first_name ILIKE 'A%' AND last_name NOT LIKE 'B%'
+--ORDER BY last_name
+
+--SELECT COUNT(amount) FROM payment
+--WHERE amount > 5;
+
+--SELECT COUNT(*) FROM actor
+--WHERE first_name LIKE 'P%'
