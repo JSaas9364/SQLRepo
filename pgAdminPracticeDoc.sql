904ba279b287c5b291729c5eb125ddd811e08ff9
Juan <46148241+JSaas9364@users.noreply.github.com>
2025-03-15 18:53:36 -0400

    updates

--- pgAdminPracticeDoc.sql
+++ pgAdminPracticeDoc.sql
@@ -10,3 +10,34 @@
 
 --SELECT COUNT(*) FROM actor
 --WHERE first_name LIKE 'P%'
+
+--SELECT COUNT(DISTINCT(district))
+--FROM address;
+
+--SELECT DISTINCT(district)
+--FROM address;
+
+--SELECT COUNT(*) FROM film
+--WHERE rating = 'R'
+--AND replacement_cost BETWEEN 5 and 15;
+
+--SELECT ROUND(AVG(replacement_cost), 2)
+--FROM film
+--SELECT ROUND(MIN(replacement_cost), 2)
+--FROM film;
+
+--SELECT ROUND(MAX(replacement_cost), 2), MIN(replacement_cost)
+--FROM film;
+
+--SELECT customer_id FROM payment
+--GROUP BY customer_id
+--ORDER BY COUNT(amount) DESC
+--ORDER BY SUM(amount) DESC
+
+--select customer_id, staff_id, sum(amount) from payment
+--group by staff_id, customer_id
+
+
+SELECT staff_id, COUNT(amount) 
+FROM payment
+GROUP BY staff_id
